5e386fbfb2f760f5266156b914cb28e38bfd401e
Rehman Nahimi <rehman2000@live.com>
2024-05-10 01:09:12 +0100

    Initial Commit

---
+++ BBC RSS view app/BBC_RSS_view_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBC_RSS_view_appApp.swift
+//  BBC RSS view app
+//
+//  Created by Ray Nahimi on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BBC_RSS_view_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
